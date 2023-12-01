d169a7b60c61ac3c0292af5f67fad18e75dfe242
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-12-01 09:01:14 +0700

    feat: linked to the links in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,38 +1,42 @@
+"use client";
+
 import {
   Menubar,
-  MenubarCheckboxItem,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarRadioGroup,
-  MenubarRadioItem,
-  MenubarSeparator,
   MenubarShortcut,
-  MenubarSub,
-  MenubarSubContent,
-  MenubarSubTrigger,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import { ModeToggle } from "./ui/mode-toggle";
 import Link from "next/link";
 import { Zap } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "./ui/use-toast";
 
 export default function Navbar() {
+  const router = useRouter();
   return (
     <Menubar className="p-6">
       <div className="w-full flex justify-between items-center">
-        <div className="flex gap-1 items-center">
+        <Link href="/" className="flex gap-1 items-center">
           <Zap size={20} />
           <MenubarMenu>LetFlash</MenubarMenu>
-        </div>
+        </Link>
         <div className="flex gap-1 items-center">
           <MenubarMenu>
             <MenubarTrigger>Create Deck</MenubarTrigger>
             <MenubarContent>
-              <MenubarItem>
-                Flashcard <MenubarShortcut>⌘T</MenubarShortcut>
+              <MenubarItem onClick={() => router.push("/create")}>
+                Deck <MenubarShortcut>⌘T</MenubarShortcut>
               </MenubarItem>
-              <MenubarItem>
+              <MenubarItem
+                onClick={() => {
+                  toast({
+                    description: "This feature is still in process!",
+                  });
+                }}
+              >
                 Upload File <MenubarShortcut>⌘N</MenubarShortcut>
               </MenubarItem>
             </MenubarContent>
