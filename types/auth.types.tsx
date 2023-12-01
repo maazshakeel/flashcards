e373995d91f3d3c0fcd2898c3e6d928219b53f66
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-12-01 13:59:22 +0700

    feat: create form schema

---
+++ types/auth.types.tsx
@@ -0,0 +1,12 @@
+import * as z from "zod";
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email address.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
+
+export { formSchema };
