30fc33b17d28bf05228d8bc16eb6f1b160ddcdaa
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-12-02 20:24:56 +0700

    feat: specify which route to sign in

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,8 @@
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { db } from "@/lib/db";
+import { User } from "@prisma/client";
 
 export const options: NextAuthOptions = {
   providers: [
@@ -8,5 +10,38 @@ export const options: NextAuthOptions = {
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
+
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email:",
+          type: "email",
+          placeholder: "Email",
+        },
+
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials) {
+        const user: any = await db.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        // If the user doesn't exist or the password is incorrect, return null
+        if (!user || user.password !== credentials?.password) {
+          return null;
+        }
+
+        // If authentication is successful, return the user object
+        return { id: user.id, email: user.email };
+      },
+    }),
   ],
+  pages: {
+    signIn: "/login",
+  },
 };
